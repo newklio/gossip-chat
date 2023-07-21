46c629b6fbb8a44e84488bf7dd209b0ef1ba18e0
deepak404found <deepakyadu404@gmail.com>
2023-07-21 14:49:34 +0530

    fixed minor bug

--- src/components/home/Feeds.tsx
+++ src/components/home/Feeds.tsx
@@ -1,8 +1,11 @@
+import { Typography } from '@mui/material'
 import React from 'react'
 
 const Feeds = () => {
     return (
-        <div>Feeds</div>
+        <Typography variant={'h1'}>
+            Feeds
+        </Typography>
     )
 }
 
