b697b534b229c27f48056bc5ded63de5cb98ff24
deepak404found <deepakyadu404@gmail.com>
2023-07-21 14:31:07 +0530

    fixed theming

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,6 @@
 import Profile from '@gossip/components/home/Profile';
 import Trending from '@gossip/components/home/Trending';
-import Feeds from '@gossip/components/home/feeds';
+import Feeds from '@gossip/components/home/Feeds';
 import MainLayout from '@gossip/components/mainLayout';
 import { Stack } from '@mui/material';
 import { grey } from '@mui/material/colors';
