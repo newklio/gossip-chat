edd0d26439a334f163f8e41d38c3e621b2f1e3f4
Aman Singh <singhaman11415@gmail.com>
2023-07-22 15:15:43 +0530

    added validation

--- src/hooks/signUp.ts
+++ src/hooks/signUp.ts
@@ -4,14 +4,39 @@ interface useSignupProps {
 	isChecked: boolean
 }
 
+interface FormData {
+	email: string
+	password: string
+	fullname: string
+}
+
+const server = process.env.NEXT_PUBLIC_API_SERVER
+
+const singUpApiCall = async (formData: FormData) => {
+	try {
+		var myHeaders = new Headers()
+		myHeaders.append('Content-Type', 'application/json')
+		// remove cors
+
+		var raw = JSON.stringify(formData)
+
+		const data = await fetch(`${server}/users/signup`, {
+			method: 'POST',
+			headers: myHeaders,
+			body: raw,
+			redirect: 'follow',
+		})
+
+		console.log(await data.json())
+	} catch (error) {
+		console.log(error)
+	}
+}
+
 export const useSignUp = ({ isChecked }: useSignupProps) => {
 	const [checked, setChecked] = useState(isChecked)
 
-	const [formData, setFormData] = useState<{
-		email: string
-		password: string
-		fullname: string
-	}>({
+	const [formData, setFormData] = useState<FormData>({
 		email: '',
 		password: '',
 		fullname: '',
@@ -21,7 +46,7 @@ export const useSignUp = ({ isChecked }: useSignupProps) => {
 		setChecked(event.target.checked)
 	}
 
-	const handleSubmit = useCallback(() => {
+	const handleSubmit = useCallback(async () => {
 		// validate form data
 		console.log(formData)
 
@@ -40,6 +65,8 @@ export const useSignUp = ({ isChecked }: useSignupProps) => {
 		if (formData.password.length <= 8) {
 			alert('Please enter a valid password (8 characters minimum)')
 		}
+
+		await singUpApiCall(formData)
 	}, [formData])
 
 	return {
