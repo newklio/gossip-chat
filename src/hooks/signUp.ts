a5b984dacb9c704826c7c9ede52ed18d7de816b1
Aman Singh <singhaman11415@gmail.com>
2023-07-22 15:39:07 +0530

    added docs for working

--- src/hooks/signUp.ts
+++ src/hooks/signUp.ts
@@ -27,6 +27,7 @@ export const useSignUp = ({ isChecked }: useSignupProps) => {
 		setChecked(event.target.checked)
 	}
 
+	// this function handles the form submission
 	const handleSubmit = useCallback(() => {
 		// validate form data
 		console.log(formData)
@@ -42,33 +43,41 @@ export const useSignUp = ({ isChecked }: useSignupProps) => {
 			alert('Please enter your email')
 			return
 		}
-
+		// password
 		if (formData.password.length <= 8) {
 			alert('Please enter a valid password (8 characters minimum)')
 		}
 
+		// creating headers for the request where we specify the content type as json
 		var myHeaders = new Headers()
 		myHeaders.append('Content-Type', 'application/json')
-		// remove cors
 
 		var raw = JSON.stringify(formData)
 
+		// sending the request to the server
 		fetch(`${server}/users/signup`, {
 			method: 'POST',
 			headers: myHeaders,
 			body: raw,
 			redirect: 'follow',
 		})
+			// if the request is successful, we redirect to the login page
 			.then(async (response) => {
+				// we parse the response body
 				let data = await response.json()
 
+				// if the response status is 200, we redirect to the login page
 				if (response.status === 200) {
 					alert('Account created')
 					router.push('/login')
-				} else {
+				}
+				// if the response status is 400, we display the error message
+				else {
 					alert(data.message || 'An error occured')
 				}
 			})
+
+			// if the request fails, we log the error
 			.catch((error) => console.log('error', error))
 	}, [formData, router])
 
