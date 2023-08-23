0e316d2868cd510036983ff7e3761618331db39c
Aman Singh <singhaman11415@gmail.com>
2023-08-23 20:22:39 +0530

    added logic to redirect from home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import { useRouter } from 'next/router'
+import React, { useEffect } from 'react'
 
 const Home = () => {
+    const router = useRouter()
+
+    useEffect(() => {
+        router.replace('/login')
+    }, [router])
     return <div>Home</div>
 }
 
